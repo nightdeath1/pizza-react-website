87dd935b93e9b118817e7f38b5ad4f0039f5e04b
nightdeath1 <86965244+nightdeath1@users.noreply.github.com>
2022-11-13 19:41:20 +0900

    Update index.jsx

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
 import {
   SidebarContainer,
   Icon,
@@ -10,33 +9,65 @@ import {
   SidebarRoute,
 } from "./SidebarElements";
 
-const Sidebar = ({ showMenu, closeMenu }) => {
+import { useSpring, animated } from 'react-spring'
 
-  const menuTransition = useSpring({
-    opacity: showMenu? 1: 0,
-    right: showMenu? -1000: 0,
-    config: {duration: 250}
-  })
+const Sidebar = ({ menuState, closeMenu }) => {
+  // const props = useSpring({
+  //   from: {
+  //     opacity: menuState ? 0 : 1,
+  //     visibility: menuState ? "hidden": "visible",
+  //   },
+  //   to: {
+  //     opacity: menuState ? 1 : 0,
+  //     visibility: menuState ? "visible": "hidden",
+  //   }
+  // });
 
-  if (!showMenu) return null;
+  // return (
+  //   <animated.div style={props}>
+  //     <SidebarContainer>
+  //             <Icon onClick={closeMenu}>
+  //               <CloseIcon />
+  //             </Icon>
+  //             <SidebarMenu>
+  //               <SidebarLink smooth to="#pizza" onClick={closeMenu}>Pizzas</SidebarLink>
+  //               <SidebarLink smooth to="#dessert" onClick={closeMenu}>Desserts</SidebarLink>
+  //               <SidebarLink to="/" onClick={closeMenu}>Full Menu</SidebarLink>
+  //             </SidebarMenu>
+  //             <SideBtnWrap>
+  //               <SidebarRoute to="/">Order</SidebarRoute>
+  //             </SideBtnWrap>
+  //           </SidebarContainer>
+  //   </animated.div>)
+  
   return (
-      <animated.div style={menuTransition}>
-      <SidebarContainer>
-        <Icon onClick={closeMenu}>
-          <CloseIcon />
-        </Icon>
-        <SidebarMenu>
-          <SidebarLink smooth to="#pizza" onClick={closeMenu}>Pizzas</SidebarLink>
-          <SidebarLink smooth to="#dessert" onClick={closeMenu}>Desserts</SidebarLink>
-          <SidebarLink to="/" onClick={closeMenu}>Full Menu</SidebarLink>
-        </SidebarMenu>
-        <SideBtnWrap>
-          <SidebarRoute to="/">Order</SidebarRoute>
-        </SideBtnWrap>
-      </SidebarContainer>
-      </animated.div>
-
+    <SidebarContainer
+      style={{
+        opacity: !menuState ? 0 : 1,
+        transition: "all 0.2s ease-in-out",
+        right: !menuState ? -1000 : 0,
+      }}
+    >
+      <Icon onClick={closeMenu}>
+        <CloseIcon />
+      </Icon>
+      <SidebarMenu>
+        <SidebarLink smooth to="#pizza" onClick={closeMenu}>
+          Pizzas
+        </SidebarLink>
+        <SidebarLink smooth to="#dessert" onClick={closeMenu}>
+          Desserts
+        </SidebarLink>
+        <SidebarLink to="/" onClick={closeMenu}>
+          Full Menu
+        </SidebarLink>
+      </SidebarMenu>
+      <SideBtnWrap>
+        <SidebarRoute to="/">Order</SidebarRoute>
+      </SideBtnWrap>
+    </SidebarContainer>
   );
+
 };
 
 export default Sidebar;
